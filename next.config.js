def2d7419f6e7027544c35f25318696e0a6f46cc
BrayanMBeltre <brayanmb07@gmail.com>
2021-07-29 02:01:24 -0400

    envs

--- next.config.js
+++ next.config.js
@@ -4,9 +4,4 @@ module.exports = {
   images: {
     domains: ["findmatthew.com"],
   },
-  env: {
-    SERVICE_ID: "service_bxoc16e",
-    TEMPLATE_ID: "template_8o2u7e8",
-    USER_ID: "user_OxL6bcHqZFeszBrBh0G20",
-  },
 };
