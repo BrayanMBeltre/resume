f98137597065e940cdd0fbbb635b33b24923352c
BrayanMBeltre <brayanmb07@gmail.com>
2021-07-29 03:24:57 -0400

    image fix

--- public/projects.js
+++ public/projects.js
@@ -1,9 +1,14 @@
 export const projects = [
   {
     title: "Kendra's Mazara Blog",
-    thumbnail: "/Thumbnail.png",
+    thumbnail: "/infraSecurityCode/Thumbnail.png",
     alt: "Infra Security Code image",
-    images: ["/Home1.png", "/Home2.png", "/Blog.png", "/About.png"],
+    images: [
+      "/infraSecurityCode/Home1.png",
+      "/infraSecurityCode/Home2.png",
+      "/infraSecurityCode/Blog.png",
+      "/infraSecurityCode/About.png",
+    ],
     description:
       "Infra Security Code is a blog made in SquareSpace for Kendra Mazara a Cybersecurity Specialist in the Dominican Republic, having a track of more than 10 years of experience in the field of Technology",
     siteLink: "https://www.infrasecuritycode.com",
