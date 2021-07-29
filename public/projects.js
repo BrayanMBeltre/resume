84e9cf3c267aa2fc37eddeb94e52ad2bc8b3c228
BrayanMBeltre <brayanmb07@gmail.com>
2021-07-29 03:24:21 -0400

    images fix

--- public/projects.js
+++ public/projects.js
@@ -1,15 +1,9 @@
 export const projects = [
   {
     title: "Kendra's Mazara Blog",
-    thumbnail:
-      "https://res.cloudinary.com/brayanmbeltre/image/upload/v1627540748/infra-security-code/Thumbnail.png",
+    thumbnail: "/Thumbnail.png",
     alt: "Infra Security Code image",
-    images: [
-      "https://res.cloudinary.com/brayanmbeltre/image/upload/v1627540769/infra-security-code/Home1.png",
-      "https://res.cloudinary.com/brayanmbeltre/image/upload/v1627540753/infra-security-code/Home2.png",
-      "https://res.cloudinary.com/brayanmbeltre/image/upload/v1627540769/infra-security-code/Blog.png",
-      "https://res.cloudinary.com/brayanmbeltre/image/upload/v1627540754/infra-security-code/About.png",
-    ],
+    images: ["/Home1.png", "/Home2.png", "/Blog.png", "/About.png"],
     description:
       "Infra Security Code is a blog made in SquareSpace for Kendra Mazara a Cybersecurity Specialist in the Dominican Republic, having a track of more than 10 years of experience in the field of Technology",
     siteLink: "https://www.infrasecuritycode.com",
